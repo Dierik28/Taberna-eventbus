b6a78cd4ad37fb7cbf192f88ecf4989ee9da0946
Dierik28 <diego.alfonso@uabc.edu.mx>
2026-04-28 01:58:25 -0700

    Se conectan los componentes mediante el EventBus y se realiza una prueba del flujo completo desde la creación de un pedido hasta la notificación de la bebida servida.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import bus.EventBus;
+import componentes.*;
+import eventos.*;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EventBus bus = new EventBus();
+
+        // Componentes
+        SistemaDePedidos pedidos = new SistemaDePedidos(bus);
+        Barra barra = new Barra(bus);
+        PanelLED panel = new PanelLED();
+
+        // Suscripciones
+        bus.subscribe(PedidoRealizadoEvent.class, barra);
+        bus.subscribe(BebidaServidaEvent.class, panel);
+
+        // Flujo
+        pedidos.crearPedido(1, 100, Arrays.asList("cerveza", "tacos"));
     }
 }
